112227273d0457a90e037a5c2a3726592166feb0
Jailson Anjos <jailson.anjos@ifood.com.br>
2022-09-09 14:05:35 -0300

    event delegation and bubbling events

--- complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +19,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +29,207 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+  console.log(
+    'height/width',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+  //Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+
+//event delegation
+
+// 1 - add event listener to common parent element
+// 2- determine what element originated the event
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  //Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+//Selecting elements
+
+console.log(document.documentElement);
+
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+document.getElementsByClassName('btn');
+
+//Selecting and inserting elements
+
+//.insertAdjacentHTML
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+//message.textContent = "We use cookies for improved functionality and analytics"
+message.innerHTML =
+  "We use cookies for improved functionality and analytics. <button class='btn btn--close-cookie'>Got it!</button>";
+
+header.prepend(message); //insert element as first child
+header.append(message); //insert element as last child. message was moved to the end of the header because
+//it can't be the first and the last element child as the same time.
+
+// header.append(message.cloneNode(true))
+
+//header.before(message);
+//header.after(message);
+
+//Delete elements
+
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+    //message.parentElement.removeChild(message);
+  });
+
+//styles
+
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+//only work for inline styles set manually by ourselves
+console.log(message.style.height); // nothing
+console.log(message.style.backgroundColor); //rgb(55, 56, 61)
+
+//for getting all the styles, we should use getComputedStyle function, pass the element as a parameter, and then,
+//choose the style:
+console.log(getComputedStyle(message).color); //rgb(187, 187, 187)
+console.log(getComputedStyle(message).height); //43.6667px
+
+// we can do operations
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+console.log(getComputedStyle(message).height);
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+//Attributes
+
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.className);
+
+logo.alt = 'Beautiful Minimalist logo';
+logo.setAttribute('company', 'Bankist');
+
+console.log(logo.src);
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.twitter-link');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+//Data Attributes
+
+console.log(logo.dataset.versionNumber);
+
+//classes
+
+// logo.classList.add('c', 'j');
+// logo.classList.remove('c');
+// logo.classList.toggle('c');
+// logo.classList.contains('c');
+
+// //don't use because it overwrites all classes
+
+// logo.classList = 'new class';
+
+const h1 = document.querySelector('h1');
+
+const alerth1 = function (e) {
+  alert('addEventListener : Bitch, you hovered me. What is wrong with you?');
+  h1.removeEventListener('mouseenter', alerth1);
+};
+
+h1.addEventListener('mouseenter', alerth1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alerth1));
+
+// h1.onmouseenter = function (e) {
+//   alert('onmouseenter: bah!');
+// };
+
+//bubbling
+
+/* 
+Most events propagate through the DOM. Event propagation has three phases:
+Capturing phase -> event is originated on the root of the DOM and it travels to the element in which the event happened.
+Imagine a link was clicked. This event is generated on the root and travels to this link, let's say, following this path:
+document -> html -> body -> section -> p -> a
+when the event hits the link, it is captured and begins the target phase
+Target phase -> the event triggers the function attached to the event listener. 
+Bubbling phase -> event travels back to the root of the DOM, triggering all event listeners related to the captured one
+*/
+
+// // rgb(255,255,255)
+// const randomInt = (min, max) =>
+//   Math.floor(Math.random() * (max - min + 1) + min);
+// const randomColor = () =>
+//   `rgb(${randomInt(0, 255)},${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+
+// document.querySelector('.nav__link').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+//   console.log('LINK', e.target, e.currentTarget);
+//   // e.currentTarget = this
+//   console.log(e.currentTarget === this);
+//   //stop propagation
+//   // e.stopPropagation();
+// });
+
+// document.querySelector('.nav__links').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+//   console.log('CONTAINER', e.target, e.currentTarget);
+// });
+
+//passing true as third argument to the event listener invert the event propagation behaviour. the event will be generated
+//its target and propagate to the DOM root
+// document.querySelector('.nav').addEventListener(
+//   'click',
+//   function (e) {
+//     this.style.backgroundColor = randomColor();
+//     console.log('NAV', e.target, e.currentTarget);
+//   } /*, true*/
+// );
