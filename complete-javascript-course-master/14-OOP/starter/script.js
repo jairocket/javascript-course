9f400be00c73ad95f94ea607f991af62a7fcd2f7
Jailson Anjos <jailson.anjos@ifood.com.br>
2022-09-18 11:24:11 -0300

    getters and setters

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -204,3 +204,89 @@ BMW.brake();
 mercedes.accelerate();
 BMW.brake();
 mercedes.brake();
+
+//ES6 classes
+
+//uses constructor functions behind the scenes
+
+//class expression
+
+// const PersonCl = class {
+
+// }
+
+//class declaration
+
+class PersonCl {
+  //Instances have only properties inside the constructor function
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  //Methods created here are set in the .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey, I'm ${this.fullName}!`);
+  }
+
+  get age() {
+    const now = new Date();
+    return now.getFullYear() - this.birthYear;
+  }
+
+  //Set property that already exists
+  set fullName(name) {
+    if (name.includes(' ')) {
+      this._fullName = name;
+    } else {
+      alert(`${name} is not a full name!`);
+    }
+  }
+  get fullName() {
+    return this._fullName;
+  }
+}
+
+const jessica = new PersonCl('Jessica Davis', 1996);
+console.log(jessica);
+jessica.calcAge();
+console.log(jessica.__proto__ === PersonCl.prototype);
+console.log('age', jessica.age);
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey, I'm ${this.firstName}!`);
+// };
+
+jessica.greet();
+
+/*
+1. Classes are not hoisted
+2. classes are first class citizens
+3. classes are executed in strict mode
+*/
+
+//Getters and Setters
+const account = {
+  owner: 'Jonas',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest); //300
+account.latest = 50;
+console.log(account.latest); //50
+
+const walter = new PersonCl('Walter White', 1965);
+
+console.log(walter);
