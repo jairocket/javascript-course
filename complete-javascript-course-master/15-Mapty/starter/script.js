86c4eb1b2959937ff0659c531a65f95f062421b6
Jailson Anjos <jailson_anjos@jailson.Anjos-Latitude-5420>
2022-09-21 22:13:09 -0300

    prettier

--- complete-javascript-course-master/15-Mapty/starter/script.js
+++ complete-javascript-course-master/15-Mapty/starter/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// prettier-ignore
+
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 const form = document.querySelector('.form');
@@ -11,6 +11,8 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent
+
 navigator.geolocation?.getCurrentPosition(
   function (position) {
     console.log(position);
@@ -18,19 +20,47 @@ navigator.geolocation?.getCurrentPosition(
     console.log(`https://www.google.com.br/maps/@${latitude},${longitude}`);
 
     console.log(L);
-    const map = L.map('map').setView([latitude, longitude], 13);
+    map = L.map('map').setView([latitude, longitude], 13);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
 
-    L.marker([latitude, longitude])
-      .addTo(map)
-      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-      .openPopup();
+    
+      map.on('click', function(mapE){
+        mapEvent = mapE
+        form.classList.remove('hidden')
+        inputDistance.focus()
+      })
   },
   function () {
     alert('Could not get your position');
   }
 );
+
+form.addEventListener('submit', function(e){
+  e.preventDefault()
+
+  inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = ''
+  const { lat, lng} = mapEvent.latlng
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250, 
+              minWidth: 100, 
+              autoClose: false, 
+              closeOnClick: false, 
+              className: 'running-popup'}
+            ))
+          .setPopupContent('Workout')
+          .openPopup();
+
+})
+
+inputType.addEventListener('change', function(){
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden')
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
+
+})
