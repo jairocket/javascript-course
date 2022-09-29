bbb5294b4422438fe59937d7e881227acb360ae2
Jailson Anjos <jailson.anjos@ifood.com.br>
2022-09-29 19:01:32 -0300

    installing dependencies with npm

--- complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
+++ complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,4 +1,5 @@
 //changing the name of imported values using 'as'
+import cloneDeep from '../../node_modules/lodash-es/cloneDeep.js';
 
 import { addToCart, totalPrice as price, qt } from './shoppingCart.js';
 
@@ -34,3 +35,20 @@ const getLastPost = async function () {
 
 const lastPost = await getLastPost();
 console.log(lastPost);
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+console.log(stateClone);
+
+state.user.loggedIn = false;
+
+console.log(stateClone.user.loggedIn); //false
+console.log(stateDeepClone.user.loggedIn); // true
