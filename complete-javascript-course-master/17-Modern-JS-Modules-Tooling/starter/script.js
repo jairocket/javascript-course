d22de1f5feef2dfa08c11dd2376396081effed78
Jailson Anjos <jailson.anjos@ifood.com.br>
2022-09-29 13:14:08 -0300

    common js modules

--- complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
+++ complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -16,3 +16,21 @@ console.log(price, qt);
 console.log(ShoppingCart.cart);
 
 add('salame', 1);
+
+//top level await works in modules
+
+//it blocks the execution of the module
+
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+  console.log(data);
+  return { title: data.at(-1).title, text: data.at(-1) };
+};
+
+const lastPost = await getLastPost();
+console.log(lastPost);
--- complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
+++ complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -1,5 +1,13 @@
 console.log('Exporting module');
 
+//blocking code
+
+// console.log('start fetching users');
+
+// await fetch('https://jsonplaceholder.typicode.com/users');
+
+// console.log('finished fetching users');
+
 const shoppingCart = 10; //|_ scoped to the module
 const cart = []; //|
 
@@ -21,3 +29,35 @@ export default function (product, quantity) {
   cart.push({ product, quantity });
   console.log(`${quantity} ${product} added to cart`);
 }
+
+//module pattern using closure
+// const ShoppingCart2 = (function () {
+//   const cart = [];
+//   const shippingCart = 10;
+//   const totalPrice = 237;
+//   const totalQuantity = 23;
+
+//   const addToCart = function (product, quantity) {
+//     cart.push({ product, quantity });
+//     console.log(`${quantity} ${product} added to cart`);
+//   };
+
+//   const orderStock = function (product, quantity) {
+//     console.log(`${quantity} ${product} ordered feom supplier`);
+//   };
+
+//   return {
+//     addToCart,
+//     cart,
+//     totalPrice,
+//     totalQuantity,
+//   };
+// })();
+
+//common js modules
+
+// export.addToCart = function (product, quantity) {
+//   cart.push({ product, quantity });
+//   console.log(`${quantity} ${product} added to cart`);
+// };
+// const {addToCart} = require('./shoppingCart.js')
